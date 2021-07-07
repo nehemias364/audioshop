fd8659b659e446732b237b6deeb84c7ff75a5af3
Nehemias astrada <nehemias364astrada@gmail.com>
2021-07-06 23:35:46 -0300

    segunda entrega navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function NavBar(){
+   return( 
+        <nav class="navbar fixed-top navbar-dark bg-dark">
+            <h1 class="brand">Audioshop</h1>
+            <ul class="navbar">
+                <li class="nav-item active">
+                    <p>Inicio</p>
+                </li>
+                <li class="nav-item active">
+                    <p>Productos</p>
+                </li>
+                <li class="nav-item active">
+                    <p>Contacto</p>
+                </li>
+            </ul>
+        </nav>
+    )
+}
